2fe28228c65444ddb24b6196f60a4dc619ef4e50
Sagnik Biswas <sagnikbiswas.official@gmail.com>
2024-01-30 20:54:40 +0530

    Initialize Sparent

---
+++ src/main/java/com/sagnik/sparent/SparentApplication.java
@@ -0,0 +1,13 @@
+package com.sagnik.sparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagnik/sparent/SparentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagnik.sparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
